83001a659cbc12160c40d1a6ba6ca6d3092f6ccf
backswan0 <rnrwprh3434@gmail.com>
2024-11-23 01:42:56 +0900

    :art: refactor: rename variables and objects to improve clarity and avoid confusion

--- src/com/example/kiosk/level2/KioskLvTwo.java
+++ src/com/example/kiosk/level2/KioskLvTwo.java
@@ -1,6 +1,6 @@
 package com.example.kiosk.level2;
 // TODO 총 2개 [A]-[B]
-// 주석 총 1개 [1]
+// 주석 총 2개 [1] - [2]
 
 import java.util.*;
 
@@ -11,19 +11,20 @@ public class KioskLvTwo {
         System.out.println("[ GRILL&THRILL MENU ]");
         List<MenuItem> menuItems = new ArrayList<>();
 
-        MenuItem itemOne = new MenuItem("BasicBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-        MenuItem itemTwo = new MenuItem("GrilledThrill", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-        MenuItem itemThree = new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-        MenuItem itemFour = new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거");
+        // [1] 객체명이 itemOne, itemTwo였으나 순서에 상관없이 쓸 수 있도록 메뉴 이름으로 변경함
+        MenuItem basicBurger = new MenuItem("BasicBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+        MenuItem grilledThrill = new MenuItem("GrilledThrill", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+        MenuItem cheeseburger = new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+        MenuItem hamburger = new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거");
 
-        // [1] 한꺼번에 저장하고 싶어서 add() 대신 Collections.addAll() 호출
-        Collections.addAll(menuItems, itemOne, itemTwo, itemThree, itemFour);
+        // [2] 한꺼번에 저장하고 싶어서 add() 대신 Collections.addAll() 호출
+        Collections.addAll(menuItems, basicBurger, grilledThrill, cheeseburger, hamburger);
         // TODO [A] Collections.addAll()보다 더 간편한 방법은 없을까?
 
-        int index = 1;
+        int menuNumber = 1;
         for (MenuItem eachItem : menuItems) {
-            eachItem.getMenuItem(index);
-            index++;
+            eachItem.getMenuItem(menuNumber);
+            menuNumber++;
         }
         System.out.println("0. 종료     | 종료");
         /* TODO [B] 처음엔 향상된 for문 안에 if문과 else문을 사용해서 종료 문구를 출력했다.
@@ -36,28 +37,28 @@ public class KioskLvTwo {
         System.out.println();
         System.out.println("메뉴판에서 원하는 햄버거 번호를 입력해 주세요.");
         System.out.print("만약 종료를 원하신다면 0을 입력해 주세요: ");
-        int menuNumber = sc.nextInt();
+        int selectedMenuNumber = sc.nextInt();
         System.out.println();
 
         // 미션
         // switch-case문을 쓰지 않고 컬렉션에서 바로 꺼내서 해보기
         // 힌트.......!!!!
-        // 리스트는 menuNumber를 인덱스로...! 써보자...!
-        switch (menuNumber) {
+        // 리스트는 selectedMenuNumber를 인덱스로...! 써보자...!
+        switch (selectedMenuNumber) {
             case 1:
-                itemOne.getChosenItem();
+                basicBurger.getChosenItem();
                 break;
 
             case 2:
-                itemTwo.getChosenItem();
+                grilledThrill.getChosenItem();
                 break;
 
             case 3:
-                itemThree.getChosenItem();
+                cheeseburger.getChosenItem();
                 break;
 
             case 4:
-                itemFour.getChosenItem();
+                hamburger.getChosenItem();
                 break;
         }
     }
