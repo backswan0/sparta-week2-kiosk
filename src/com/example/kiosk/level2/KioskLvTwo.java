155825d9f2806f0aec6c6bb7050734883d0cdcdd
backswan0 <rnrwprh3434@gmail.com>
2024-11-25 01:19:57 +0900

    :bulb: chore: remove and update unnecessary comments

--- src/com/example/kiosk/level2/KioskLvTwo.java
+++ src/com/example/kiosk/level2/KioskLvTwo.java
@@ -1,6 +1,6 @@
 package com.example.kiosk.level2;
 // TODO 총 2개 [A]-[B]
-// 주석 총 2개 [1] - [2]
+// 주석 총 3개 [1]-[3]
 
 import java.util.*;
 
@@ -11,22 +11,20 @@ public class KioskLvTwo {
         System.out.println("[ GRILL&THRILL MENU ]");
         List<MenuItem> menuItems = new ArrayList<>();
 
-        // [1] 객체명이 itemOne, itemTwo였으나 순서에 상관없이 쓸 수 있도록 메뉴 이름으로 변경함
         MenuItem basicBurger = new MenuItem("BasicBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거");
         MenuItem grilledThrill = new MenuItem("GrilledThrill", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
         MenuItem cheeseburger = new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
         MenuItem hamburger = new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거");
 
-        // [2] 한꺼번에 저장하고 싶어서 add() 대신 Collections.addAll() 호출
         Collections.addAll(menuItems, basicBurger, grilledThrill, cheeseburger, hamburger);
         // TODO [A] Collections.addAll()보다 더 간편한 방법은 없을까?
 
         int menuNumber = 1;
         for (MenuItem eachItem : menuItems) {
-            eachItem.getMenuItem(menuNumber);
+            eachItem.getMenuInfo(menuNumber);
             menuNumber++;
         }
-        System.out.println("0. 종료     | 종료");
+        System.out.println("0. 종료           | 종료");
         /* TODO [B] 처음엔 향상된 for문 안에 if문과 else문을 사용해서 종료 문구를 출력했다.
             문제는 리스트가 추가됐을 때 종료 문구가 맨 마지막에 나오지 않아 번거로웠다.
             이에, 지금처럼 반복문이 끝나는 시점에 종료 문구를 출력해서 해결했다.
@@ -34,32 +32,34 @@ public class KioskLvTwo {
             어떻게 하면 좀 더 간단하게 생각할 수 있을지 고민이 된다.
          */
 
-        System.out.println();
-        System.out.println("메뉴판에서 원하는 햄버거 번호를 입력해 주세요.");
-        System.out.print("만약 종료를 원하신다면 0을 입력해 주세요: ");
-        int selectedMenuNumber = sc.nextInt();
-        System.out.println();
+        while (true) {
+            System.out.println();
+            System.out.println("메뉴판에서 원하는 햄버거 번호를 입력해 주세요.");
+            System.out.print("만약 종료를 원하신다면 0을 입력해 주세요: ");
+            int selectedMenuNumber = sc.nextInt();
 
-        // 미션
-        // switch-case문을 쓰지 않고 컬렉션에서 바로 꺼내서 해보기
-        // 힌트.......!!!!
-        // 리스트는 selectedMenuNumber를 인덱스로...! 써보자...!
-        switch (selectedMenuNumber) {
-            case 1:
-                basicBurger.getChosenItem();
+            /*
+            [1] 논리 연산자(||)를 사용한 이유
+            사용자가 0 외에 다른 번호를 누를 가능성을 고려하여 사용함
+            */
+            if (selectedMenuNumber >= 5 || selectedMenuNumber == 0) {
+                System.out.println("메뉴 번호 외에 다른 숫자를 입력했습니다. 프로그램을 종료합니다.");
                 break;
-
-            case 2:
-                grilledThrill.getChosenItem();
-                break;
-
-            case 3:
-                cheeseburger.getChosenItem();
-                break;
-
-            case 4:
-                hamburger.getChosenItem();
-                break;
+            } else {
+                /*
+                [2] 변수 index를 선언하고 초기화하여 사용한 이유
+                첫째, selectedMenuNumber와 구분하고자
+                둘째, 메서드의 인데스라는 점을 분명히 하고자
+                */
+                int index = selectedMenuNumber - 1;
+                System.out.println();
+                menuItems.get(index).getEachItem();
+                /*
+                [3] switch-case문을 사용하지 않은 이유
+                첫째, 1단계 키오스크에서 switch-case문을 실제로 사용했고
+                둘째, List로도 같은 효과를 줄 수 있다는 튜터님의 제안으로 List를 활용함
+                */
+            }
         }
     }
 }
--- src/com/example/kiosk/level2/MenuItem.java
+++ src/com/example/kiosk/level2/MenuItem.java
@@ -1,12 +1,7 @@
 package com.example.kiosk.level2;
-// 주석 총 2개 [1] - [2]
 
 public class MenuItem {
 
-    /*
-    [1] private int menuNum이 없어도 파라미터 int menuNum을 활용하면 됨
-    private int menuNum;
-     */
     private String name;
     private double price;
     private String description;
@@ -17,17 +12,12 @@ public class MenuItem {
         this.description = description;
     }
 
-    public void getMenuItem(int menuNum) {
-        /*
-        [2] [1]과 같은 이유로 수정함. 주석 처리한 이유는 복습을 위해
-        this.menuNum = menuNum;
-        System.out.println(this.menuNum + ". " + this.name + "     | W " + this.price + " | " + this.description);
-         */
+    public void getMenuInfo(int menuNum) {
         System.out.println(menuNum + ". " + this.name + "     | W " + this.price + " | " + this.description);
     }
 
-    public void getChosenItem() {
+    public void getEachItem() {
         System.out.println(this.name + " 1개가 선택되었습니다.");
-        System.out.println("가격은 " + (int)(this.price * 1000) + "원입니다.");
+        System.out.println("가격은 " + (int) (this.price * 1000) + "원입니다.");
     }
 }
