342811172283b75f3f1ac8f94bea8263af6e0b11
backswan0 <rnrwprh3434@gmail.com>
2024-11-25 14:38:49 +0900

    :wrench: refactor: remove int i and use the member variable private int index, and add comments explaining two warning messages

--- src/com/example/kiosk/level3/Kiosk.java
+++ src/com/example/kiosk/level3/Kiosk.java
@@ -7,7 +7,11 @@ public class Kiosk {
     // 1. 속성 (== 멤버 변수)
     Scanner sc = new Scanner(System.in);
     List<MenuItem> menuItems = new ArrayList<>();
+    // 행동이....겹치네....??? List.of로 해주는 데???
+    // 외부에서 주입되는 방식이 이미 있는데 내부에서 또?????
     private int index;
+    // 인덱스라는 변수를 멤버변수로 선언을 했는데, 사용되는 형태가 메서드의 안에서만 사용되고 있으니까 굳이...? 이런 의미
+    // 지역 변수로 하지 그래..? 라고 제안하는 것
 
     // 2. 생성자
     public Kiosk(List<MenuItem> menuItems) {
@@ -15,15 +19,26 @@ public class Kiosk {
     }
 
     // 3. 기능 (== 메서드)
-    public void start () {
-        for (int i = 0; i<this.menuItems.size(); i++) {
-            this.index = i;
-            this.menuItems.get(this.index).getMenuInfo(this.index +1);
+    public void start() {
+        for (index = 0; index < this.menuItems.size(); index++) {
+            this.menuItems.get(index).getMenuInfo(index + 1);
         }
         System.out.println("0. 종료           | 종료");
         // TODO [A] 처음에는 향상된 for문을 쓰려고 했는데 인덱스 표시가 어려워서 for문으로 방향을 틀었다.
         //          튜터님이 while문과 향상된 for문 버전을 보여주실 때 깨달았다.
         //          다양한 방식을 쓸 수 있어야 상황에 맞게 골라 쓸 수 있다는 사실을.
         //          내가 for문으로 방향을 튼 이유가 '그냥'이 아니었다는 사실 또한.
+
+        System.out.println();
+        System.out.println("메뉴판에서 원하는 햄버거 번호를 입력해 주세요.");
+        System.out.print("만약 종료를 원하신다면 0을 입력해 주세요: ");
+        index = sc.nextInt();
+
+        if (index >= 5 || index == 0) {
+            System.out.println("메뉴 번호 외에 다른 숫자를 입력했습니다. 프로그램을 종료합니다.");
+        } else {
+            System.out.println();
+            menuItems.get(index - 1).getEachItem();
+        }
     }
 }
