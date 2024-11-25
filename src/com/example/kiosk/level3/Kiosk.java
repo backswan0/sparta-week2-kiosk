df45458712734ecfbe52fe7e0c2d97d65338f9f8
backswan0 <rnrwprh3434@gmail.com>
2024-11-25 20:22:26 +0900

    :bulb: chore: clean up unnecessary comments

--- src/com/example/kiosk/level3/Kiosk.java
+++ src/com/example/kiosk/level3/Kiosk.java
@@ -1,36 +1,22 @@
 package com.example.kiosk.level3;
-// TODO 총 1개 [A]
-
+// 주석 총 3개 [1] - [3]
 import java.util.*;
 
 public class Kiosk {
-    // 1. 속성 (== 멤버 변수)
     Scanner sc = new Scanner(System.in);
-    List<MenuItem> menuItems = new ArrayList<>();
-    // 경고(1)
-    // '행동이....겹치네....??? List.of로 해주는 데???'
-    // '외부에서 주입되는 방식이 이미 있는데 내부에서 또?????'
-    // 그래서 불필요하다고 인텔리제이가 얘기해준 것
-    private int index;
-    // 경고(2)
-    // 인덱스라는 변수를 멤버변수로 선언을 했는데, 사용되는 형태가 메서드의 안에서만 사용되고 있으니까 '굳이...?' 이런 의미
-    // '지역 변수로 선언해서 쓰지 그래?'라고 인텔리제이가 제안한 것
+    List<MenuItem> menuItems;
 
-    // 2. 생성자
     public Kiosk(List<MenuItem> menuItems) {
         this.menuItems = menuItems;
     }
 
-    // 3. 기능 (== 메서드)
     public void start() {
+        // [1] 메서드 안에서만 쓰기 때문에 index를 지역 번수로 선언함
+        int index;
         for (index = 0; index < this.menuItems.size(); index++) {
             this.menuItems.get(index).getMenuInfo(index + 1);
         }
         System.out.println("0. 종료           | 종료");
-        // TODO [A] 처음에는 향상된 for문을 쓰려고 했는데 인덱스 표시가 어려워서 for문으로 방향을 틀었다.
-        //          튜터님이 while문과 향상된 for문 버전을 보여주실 때 깨달았다.
-        //          다양한 방식을 쓸 수 있어야 상황에 맞게 골라 쓸 수 있다는 사실을.
-        //          내가 for문으로 방향을 튼 이유가 '그냥'이 아니었다는 사실 또한.
 
         while (true) {
             System.out.println();
@@ -38,9 +24,12 @@ public class Kiosk {
             System.out.print("만약 종료를 원하신다면 0을 입력해 주세요: ");
             index = sc.nextInt();
 
+            // [2] 사용자가 메뉴에 없는 숫자를 누를 상황을 고려하여 index >= 5 조건을 추가함
             if (index >= 5 || index == 0) {
                 System.out.println("메뉴 번호 외에 다른 숫자를 입력했습니다. 프로그램을 종료합니다.");
                 break;
+
+            // [3] 사용자가 누르는 숫자는 index보다 1이 더 크기 때문에 'index -1'로 작성함
             } else {
                 System.out.println();
                 menuItems.get(index - 1).getEachItem();
--- src/com/example/kiosk/level3/KioskLvThree.java
+++ src/com/example/kiosk/level3/KioskLvThree.java
@@ -1,20 +1,11 @@
 package com.example.kiosk.level3;
 // 주석 총 1개 [A]
-
 import java.util.*;
 
 public class KioskLvThree {
     public static void main(String[] args) {
-        /*
-        [A]
-        - 처음에는 add()를 활용했으나 가독성을 고려하여 List.of() 사용함
-        - 초안
-        List<MenuItem> menuItems = new ArrayList<MenuItem>();
-        menuItems.add(new MenuItem("BasicBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("GrilledThrill", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
-         */
+
+        // [A] 가독성을 고려하여 add() 대신 List.of() 사용함
         List<MenuItem> menuItems = List.of(
                 new MenuItem("BasicBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"),
                 new MenuItem("GrilledThrill", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"),
@@ -22,8 +13,8 @@ public class KioskLvThree {
                 new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거")
         );
 
-        Kiosk test = new Kiosk(menuItems);
+        Kiosk kiosk = new Kiosk(menuItems);
 
-        test.start();
+        kiosk.start();
     }
 }
