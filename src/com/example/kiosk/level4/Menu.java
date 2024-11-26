5626e48b414479f8477e3a6d30daec421c59903a
backswan0 <rnrwprh3434@gmail.com>
2024-11-27 00:25:44 +0900

    :art: refactor: rename all the variables and parameters in the Menu class

--- src/com/example/kiosk/level4/Menu.java
+++ src/com/example/kiosk/level4/Menu.java
@@ -3,17 +3,17 @@ package com.example.kiosk.level4;
 import java.util.*;
 
 public class Menu {
-    List<MenuItem> listOfMenuItem;
-    public String nameOfMenu;
+    List<MenuItem> menuItemList;
+    public String menuName;
 
-    public Menu(String nameOfMenu, List<MenuItem> listOfMenuItem) {
-        this.nameOfMenu = nameOfMenu;
-        this.listOfMenuItem = listOfMenuItem;
+    public Menu(String menuName, List<MenuItem> menuItemList) {
+        this.menuName = menuName;
+        this.menuItemList = menuItemList;
     }
 
     public void displayAllMenuItems() {
-        for (int i = 0; i < this.listOfMenuItem.size(); i++) {
-            this.listOfMenuItem.get(i).displayEachMenuItem(i);
+        for (int i = 0; i < this.menuItemList.size(); i++) {
+            this.menuItemList.get(i).displayEachMenuItem(i);
             // TODO [A] 어떤 지역 변수를 새롭게 선언해야 좀 더 간단하게 쓸 수 있을까?
         }
     }
