56c43ad9669973aaefb69fa69d00ec8e44bf2fcd
backswan0 <rnrwprh3434@gmail.com>
2024-11-26 09:35:52 +0900

    :page_facing_up: draft: outline member variables, a constructor, and a method for the MenuItem class

---
+++ src/com/example/kiosk/level4/MenuItem.java
@@ -0,0 +1,20 @@
+package com.example.kiosk.level4;
+
+public class MenuItem {
+
+    private String name;
+    private double price;
+    private String description;
+
+    public MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public void getMenuInfo(int menuNum) {
+        System.out.print(menuNum + ". " + this.name);
+        System.out.print("      | W " + this.price);
+        System.out.println(" | " + this.description);
+    }
+}
