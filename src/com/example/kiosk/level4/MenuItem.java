b3b24a60e720cc38d9d80dd0564820aa8fe21937
backswan0 <rnrwprh3434@gmail.com>
2024-11-27 00:24:15 +0900

    :art: refactor: rename all the variables and parameters in the MenuItem class

--- src/com/example/kiosk/level4/MenuItem.java
+++ src/com/example/kiosk/level4/MenuItem.java
@@ -2,20 +2,21 @@ package com.example.kiosk.level4;
 
 public class MenuItem {
 
-    public String nameOfMenuItem;
-    public double costOfMenuItem;
-    public String infoOfMenuItem;
+    public String menuItemName;
+    public double menuItemCost;
+    public String menuItemInfo;
 
-    public MenuItem(String nameOfMenuItem, double costOfMenuItem, String infoOfMenuItem) {
-        this.nameOfMenuItem = nameOfMenuItem;
-        this.costOfMenuItem = costOfMenuItem;
-        this.infoOfMenuItem = infoOfMenuItem;
+    public MenuItem(String menuItemName, double menuItemCost, String menuItemInfo) {
+        this.menuItemName = menuItemName;
+        this.menuItemCost = menuItemCost;
+        this.menuItemInfo = menuItemInfo;
     }
 
-    public void displayEachMenuItem(int indexOfMenuItem) {
-        int numberOfMenuItem = indexOfMenuItem + 1;
-        System.out.print(numberOfMenuItem + ". " + this.nameOfMenuItem);
-        System.out.print("      | W " + this.costOfMenuItem);
-        System.out.println(" | " + this.infoOfMenuItem);
+    public void displayEachMenuItem(int menuItemIndex) {
+        // [1] Menu의 i가 int menuItemIndex에 해당한다.
+        int menuItemNumber = menuItemIndex + 1;
+        System.out.print(menuItemNumber + ". " + this.menuItemName);
+        System.out.print("      | W " + this.menuItemCost);
+        System.out.println(" | " + this.menuItemInfo);
     }
 }
