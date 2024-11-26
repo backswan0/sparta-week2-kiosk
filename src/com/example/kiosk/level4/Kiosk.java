ad8ca7a6279bac815f3f49bf6835eddc0475de98
backswan0 <rnrwprh3434@gmail.com>
2024-11-26 09:50:07 +0900

    :page_facing_up: draft: outline a member variable and a constructor for the Kiosk class

---
+++ src/com/example/kiosk/level4/Kiosk.java
@@ -0,0 +1,16 @@
+package com.example.kiosk.level4;
+
+import java.util.*;
+
+public class Kiosk {
+    // 1. 속성
+    List<Menu> menuList;
+
+    // 2. 생성자
+    public Kiosk (List<Menu> menuList) {
+        this.menuList = menuList;
+    }
+
+    // 3. 기능
+
+}
