03fca764f37ff5444c451e5163d5fbc64dfcbf37
backswan0 <rnrwprh3434@gmail.com>
2024-11-26 20:19:21 +0900

    :sparkles: feat: add a new method in the Menu class

--- src/com/example/kiosk/level4/Menu.java
+++ src/com/example/kiosk/level4/Menu.java
@@ -1,13 +1,20 @@
 package com.example.kiosk.level4;
-
+// TODO 총 1개 [A]
 import java.util.*;
 
 public class Menu {
-    List<MenuItem> listOfMenu;
+    List<MenuItem> listOfMenuItem;
     public String nameOfMenu;
 
-    public Menu(String nameOfMenu, List<MenuItem> listOfMenu) {
+    public Menu(String nameOfMenu, List<MenuItem> listOfMenuItem) {
         this.nameOfMenu = nameOfMenu;
-        this.listOfMenu = listOfMenu;
+        this.listOfMenuItem = listOfMenuItem;
+    }
+
+    public void displayAllMenuItems() {
+        for (int i = 0; i < this.listOfMenuItem.size(); i++) {
+            this.listOfMenuItem.get(i).displayEachMenuItem(i);
+            // TODO [A] 어떤 지역 변수를 새롭게 선언해야 좀 더 간단하게 쓸 수 있을까?
+        }
     }
 }
