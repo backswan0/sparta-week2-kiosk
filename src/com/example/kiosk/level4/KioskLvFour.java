a448de81176370ed69c966cf26a932c090881bec
backswan0 <rnrwprh3434@gmail.com>
2024-11-26 16:01:42 +0900

    :memo: docs: add comments to main method and test start() method of the Kiosk class

--- src/com/example/kiosk/level4/KioskLvFour.java
+++ src/com/example/kiosk/level4/KioskLvFour.java
@@ -9,8 +9,14 @@ public class KioskLvFour {
         List<Menu> menuList = new ArrayList<>();
         // 장바구니 기능을 추가하면 메뉴를 넣거나 빼야 하는데 List로는 변경이 불가능해서 ArrayList 사용함
         // 질문: initialCapacity 부분에 값을 입력하나요, 마나요..........?
+        // 코딩은 먼저 써보고 찾아보자 외워서 하기보다는..!
+
         Menu menuBurger = new Menu("Burgers", new ArrayList<>(3));
 
+        // 컬렉션의 메서드는 구글해서 기능을 찾는 연습도 해보자..!
+        // 자료구조의 특징과 키워드를 같이 연관지어서 (stack은 선입후출이라 push..! 이런 식으로)
+        // 이 부분을 바꿔야 5단계 == 캡슐화..! 이 부분을 인지하면 된다..!
+        // 깃허브에서도 코드 구경 많이 하자!
         menuBurger.menuItemList.add(0, new MenuItem("B1", 1.0, "불고기버거"));
         menuBurger.menuItemList.add(1, new MenuItem("B2", 2.0, "치킨버거"));
         menuBurger.menuItemList.add(2, new MenuItem("B3", 3.0, "새우버거"));
