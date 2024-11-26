8e123e7bdf24f186fbe9b5213b6f1f73b294a6e3
backswan0 <rnrwprh3434@gmail.com>
2024-11-26 19:30:21 +0900

    :wastebasket: remove: delete unnecessary method from Menu class when displaying main menus in main method

--- src/com/example/kiosk/level4/Menu.java
+++ src/com/example/kiosk/level4/Menu.java
@@ -3,23 +3,11 @@ package com.example.kiosk.level4;
 import java.util.*;
 
 public class Menu {
-    // 1. 속성
-    List<MenuItem> menuItemList;
-    public String name;
+    List<MenuItem> listOfMenu;
+    public String nameOfMenu;
 
-    // 2. 생성자
-    public Menu(String name, List<MenuItem> menuItemList) {
-        this.name = name;
-        this.menuItemList = menuItemList;
-    }
-
-    // 3. 기능
-    // 버거를 고르면 버거들만 쭉 나오는 메서드 추가해야 함...!
-    public void getMenuItem () {
-        int i;
-        for (i = 0; i<this.menuItemList.size(); i++) {
-            this.menuItemList.get(i).getMenuInfo(i);
-        }
-
+    public Menu(String nameOfMenu, List<MenuItem> listOfMenu) {
+        this.nameOfMenu = nameOfMenu;
+        this.listOfMenu = listOfMenu;
     }
 }
