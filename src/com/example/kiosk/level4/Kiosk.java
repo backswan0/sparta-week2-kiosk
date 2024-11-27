409be7e936e09ad7e14c8e0debaf098406f55f23
backswan0 <rnrwprh3434@gmail.com>
2024-11-27 19:55:10 +0900

    :sparkles: refactor: move an operation written in the start() method to the Menu class and call it

--- src/com/example/kiosk/level4/Kiosk.java
+++ src/com/example/kiosk/level4/Kiosk.java
@@ -1,7 +1,6 @@
 package com.example.kiosk.level4;
 
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Kiosk {
     public Scanner sc = new Scanner(System.in);
@@ -18,45 +17,28 @@ public class Kiosk {
 
             inputNumber();
 
-            int j = inputNumber - 1;
-            if ((inputNumber > 0) && (inputNumber <= this.menus.size())) {
-                this.menus.get(j).displayAllMenuItems();
+            int i = inputNumber - 1;
+            if ((0 < inputNumber) && (inputNumber <= menus.size())) {
+                menus.get(i).displayAllItems();
             } else if (inputNumber == 0) {
                 System.out.println("0을 입력하셨습니다. 프로그램을 종료합니다.");
                 break;
             } else {
-                System.out.println("메인에 없는 번호를 입력하셨습니다. 프로그램을 종료합니다. ");
+                System.out.println("메뉴판에 없는 번호를 입력하셨습니다. 프로그램을 종료합니다. ");
                 break;
             }
 
             inputNumber();
 
-            int k = inputNumber - 1;
-            int menuItemListSize = this.menus.get(j).menuItemList.size();
-            if ((inputNumber > 0) && (inputNumber <= menuItemListSize)) {
-                this.menus.get(j).menuItemList.get(k).displaySelectedMenuItem();
-                System.out.print("뒤로 가기를 원하시면 0을 입력해 주세요: ");
-                inputNumber = sc.nextInt();
-                if (inputNumber == 0) {
-                    System.out.println();
-                    continue;
-                }
-            } else if (inputNumber == 0) {
-                System.out.println("0을 입력하셨습니다. 프로그램을 종료합니다.");
-                break;
-            } else {
-                System.out.println("메뉴에 없는 번호를 입력하셨습니다. 메인 메뉴로 돌아갑니다.");
-                System.out.println();
-                continue;
-            }
+            menus.get(i).displaySelectedItem(inputNumber);
         }
     }
 
     public void displayMenus() {
         System.out.println("[ MAIN MENU ]");
-        for (int i = 0; i < menus.size(); i++) {
-            int menuNumber = i + 1;
-            String menuName = menus.get(i).menuName;
+        for (int j = 0; j < menus.size(); j++) {
+            int menuNumber = j + 1;
+            String menuName = menus.get(j).menuName;
             System.out.println(menuNumber + ". " + menuName);
         }
         System.out.println("0. 종료           | 종료");
