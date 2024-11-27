dfa2d4dbecb05cde5aa066b46517a76b92577f82
backswan0 <rnrwprh3434@gmail.com>
2024-11-27 10:51:38 +0900

    :wrench: refactor: modify if condition in start() method of Kiosk class and record feedback

--- src/com/example/kiosk/level4/Kiosk.java
+++ src/com/example/kiosk/level4/Kiosk.java
@@ -1,5 +1,5 @@
 package com.example.kiosk.level4;
-// 주석 총 3개 [1] - [3]
+// 주석 총 3개 [1] - [4]
 
 import java.util.*;
 
@@ -20,6 +20,11 @@ public class Kiosk {
     혹시라도 지역 변수와 헷갈릴까 봐 더 확실히 구분하고자
      */
     public void start() {
+        // 함수 쪼개자..!!!!!
+        // 어쩐지 God method로 가는 듯하더라니..!!!
+        // 서론 본론 결론 나누듯이..!
+        // 두 가지 : 메뉴 처리 / 메뉴 아이템 처리
+        // 구조화 하는 연습을 많이 해보자! (입력/출력은 사용자와 상호작용하는 역할 -> 이게 바로 객체..!
         for (int i = 0; i < this.list.size(); i++) {
             // [3] 인덱스와 구분하고자 지역 변수를 새로 선언 및 초기화함
             int menuNumber = i + 1;
@@ -29,33 +34,38 @@ public class Kiosk {
         System.out.println("0. 종료           | 종료");
 
         System.out.println();
+        // 반복문이 없으면 놓칠 수 있는 부분: size 이상 번호가 들어왔을 때
 
-        System.out.print("[MAIN MENU]에서 원하는 메뉴를 골라주세요. 0을 누르면 프로그램이 종료됩니다: ");
-        int j = 0;
-        int inputNumber;
-        inputNumber = sc.nextInt();
-        if (inputNumber == 0) {
-            System.out.println("0을 입력하셨습니다. 프로그램을 종료합니다.");
-            System.exit(0);
-        } else if (inputNumber > this.list.size()) {
-            System.out.println("[MAIN MENU]에 없는 번호를 입력하셨습니다. 프로그램을 종료합니다.");
-            System.exit(0);
-        } else {
-            j = inputNumber - 1;
+        System.out.print("[MAIN MENU]에 있는 번호를 입력해 주세요. 0을 누르면 프로그램이 종료됩니다: ");
+        int inputNumber = sc.nextInt();
+        System.out.println();
+        int j = inputNumber - 1;
+        if ((inputNumber > 0) && (inputNumber <= this.list.size())) {
+            // selectedMenu로 변수 하나 설정해서 써보자
+            // this.list.get(j)
             this.list.get(j).displayAllMenuItems();
+        } else if (inputNumber == 0) {
+            System.out.println("0을 입력하셨습니다. 프로그램을 종료합니다.");
+        } else {
+            System.out.println("메인에 없는 번호를 입력하셨습니다. 프로그램을 종료합니다. ");
+            System.exit(0);
         }
 
         System.out.println();
+
         System.out.print("원하는 메뉴의 번호를 입력해 주세요. 0을 누르면 프로그램이 종료됩니다: ");
         inputNumber = sc.nextInt();
-        if (inputNumber == 0) {
+        int k = inputNumber - 1;
+        int menuItemListSize = this.list.get(j).menuItemList.size();
+        if ((inputNumber > 0) && (inputNumber <= menuItemListSize)) {
+            this.list.get(j).menuItemList.get(k).displaySelectedMenuItem();
+            // 최적화할 수 있는 포인트가 있을 듯
+        } else if (inputNumber == 0) {
             System.out.println("0을 입력하셨습니다. 프로그램을 종료합니다.");
             System.exit(0);
-        } else if (inputNumber > this.list.get(j).menuItemList.size()) {
+        } else {
             System.out.println("메뉴에 없는 번호를 입력하셨습니다. 프로그램을 종료합니다.");
             System.exit(0);
-        } else {
-            System.out.println("사용자가 선택한 MenuItem이 출력될 부분으로, 현재 구현 중입니다.");
         }
     }
 }
