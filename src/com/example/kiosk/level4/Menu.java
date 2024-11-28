169749a845dc1f5d5faecc7c87d384e004584cb3
backswan0 <rnrwprh3434@gmail.com>
2024-11-28 09:14:47 +0900

    :memo: docs: add a comment in the Menu class and update access modifier

--- src/com/example/kiosk/level4/Menu.java
+++ src/com/example/kiosk/level4/Menu.java
@@ -1,11 +1,11 @@
 package com.example.kiosk.level4;
-// 주석 총 1개 [1]
+// 주석 총 2개 [1] - [2]
 // TODO 총 1개 [A]
 
 import java.util.*;
 
 public class Menu {
-    List<MenuItem> items;
+    public List<MenuItem> items;
     public String menuName;
 
     public Menu(String menuName, List<MenuItem> items) {
@@ -23,6 +23,7 @@ public class Menu {
         System.out.println("0. 뒤로가기        | 뒤로가기");
     }
 
+    // [1] 선택된 아이템은 Menu 클래스에서 관리하는 게 맞다고 판단하여 메서드를 추가함
     public void displaySelectedItem(int number) {
         int size = items.size();
         if (number == 0) {
@@ -38,6 +39,6 @@ public class Menu {
         } else if (size < number) {
             System.out.println("메뉴판에 없는 번호를 입력하셨습니다.메인으로 돌아갑니다.");
             System.out.println();
-        } // [1] 안내 문구와 메뉴 사이에 한 줄씩 여백을 두고 싶어서 System.out.println(); 추가함
+        } // [2] 안내 문구와 메뉴 사이에 한 줄씩 여백을 두고 싶어서 System.out.println(); 추가함
     }
 }
